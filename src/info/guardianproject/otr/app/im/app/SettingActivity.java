8a5db6b52f1e10f67a78426d411896712dcecfd6
Hans-Christoph Steiner <hans@eds.org>
2011-03-31 16:57:37 -0400

    fixed setting of EditTexts and made values show as the 'summary' in the overview

--- src/info/guardianproject/otr/app/im/app/SettingActivity.java
+++ src/info/guardianproject/otr/app/im/app/SettingActivity.java
@@ -17,140 +17,199 @@
 
 package info.guardianproject.otr.app.im.app;
 
+import info.guardianproject.otr.app.im.R;
 import info.guardianproject.otr.app.im.provider.Imps;
 import info.guardianproject.otr.app.im.service.ImServiceConstants;
-
-import info.guardianproject.otr.app.im.R;
-
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.EditTextPreference;
-import android.preference.Preference;
-import android.preference.PreferenceManager;
-import android.preference.PreferenceScreen;
+import android.preference.ListPreference;
 import android.util.Log;
 import android.widget.Toast;
 
-public class SettingActivity extends android.preference.PreferenceActivity {
+public class SettingActivity extends android.preference.PreferenceActivity implements OnSharedPreferenceChangeListener {
 
     private long mProviderId;
     private long mAccountId;
 
+    EditTextPreference mUser;
+    EditTextPreference mPass;
+    EditTextPreference mDomain;
+    EditTextPreference mXmppResource;
+    EditTextPreference mPort;
+    EditTextPreference mServer;
+    ListPreference mOtrMode;
+    CheckBoxPreference mAllowPlainAuth;
+    CheckBoxPreference mRequireTls;
+    CheckBoxPreference mTlsCertVerify;
+    CheckBoxPreference mDoDnsSrv;
+    CheckBoxPreference mHideOfflineContacts;
+    CheckBoxPreference mEnableNotification;
+    CheckBoxPreference mNotificationVibrate;
+    CheckBoxPreference mNotificationSound;
+    
+    private void setInitialValues() {
+		ContentResolver cr = getContentResolver();
+        Imps.ProviderSettings.QueryMap settings = new Imps.ProviderSettings.QueryMap(
+                cr, mProviderId,
+                false /* keep updated */, null /* no handler */);        
+        String text;
+
+        final String pass = Imps.Account.getPassword(cr, mAccountId);
+        if (pass != null) mUser.setText(pass);
+        text = Imps.Account.getUserName(cr, mAccountId);
+        if (text != null) {
+        	mUser.setSummary(text);
+            mUser.setText(text);
+        }
+        text = settings.getDomain();
+        if (text != null) {
+        	mDomain.setSummary(text);
+            mDomain.setText(text);
+        }
+        text = settings.getXmppResource();
+        if (text != null) {
+        	mXmppResource.setSummary(text);
+        	mXmppResource.setText(text);
+        }
+        text = Integer.toString(settings.getPort());
+        if (text != null && text != "5222") {
+            mPort.setSummary(text);
+            mPort.setText(text);
+        }
+        text = settings.getServer();
+        if (text != null) {
+        	mServer.setSummary(text);
+        	mServer.setText(text);
+        }
+        mOtrMode.setValue(settings.getOtrMode());
+        mAllowPlainAuth.setChecked(settings.getAllowPlainAuth());
+        mRequireTls.setChecked(settings.getRequireTls());
+        mTlsCertVerify.setChecked(settings.getTlsCertVerify());
+        mDoDnsSrv.setChecked(settings.getDoDnsSrv());
+        mHideOfflineContacts.setChecked(settings.getHideOfflineContacts());
+        // TODO setting mEnableNofitication causes null pointer exception
+        //mEnableNotification.setChecked(settings.getEnableNotification());
+        mNotificationVibrate.setChecked(settings.getVibrate());
+        mNotificationSound.setChecked(settings.getRingtoneURI() != null);
+        
+        settings.close();
+    }
+
+    /* save the preferences in Imps so they are accessible everywhere */
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+    	final Imps.ProviderSettings.QueryMap settings = new Imps.ProviderSettings.QueryMap(
+    			getContentResolver(), mProviderId,
+    			false /* don't keep updated */, null /* no handler */);
+    	String value;
+    	if (key.equals(getString(R.string.pref_account_user)) || key.equals(getString(R.string.pref_account_pass))) {
+    		ContentResolver cr = getContentResolver();
+    		String user = prefs.getString(getString(R.string.pref_account_user), null);
+    		final String pass = prefs.getString(getString(R.string.pref_account_pass), null);
+    		mAccountId = ImApp.insertOrUpdateAccount(cr, mProviderId, user, pass);
+    		if (user != null) mUser.setSummary(user);
+    	} else if (key.equals(getString(R.string.pref_account_domain))) {
+    		value = prefs.getString(key, null);
+    		if (value != null) {
+    			settings.setDomain(value);
+    			mDomain.setSummary(value);
+    		}
+    	} else if (key.equals(getString(R.string.pref_account_xmpp_resource))) {
+    		value = prefs.getString(key, null);
+    		if (value != null) {
+        		settings.setXmppResource(value);
+    			mXmppResource.setSummary(value);
+    		}
+    	} else if (key.equals(getString(R.string.pref_account_port))) {
+    		value = prefs.getString(key, "5222");
+    		try {
+    			settings.setPort(Integer.parseInt(value));
+    		} catch (NumberFormatException nfe) {
+    			// TODO port numbers with non-int content should be handled better
+    			Toast.makeText(getBaseContext(), "Port number must be a number",
+    					Toast.LENGTH_SHORT).show();     
+    		}
+    		if (value != "5222") mPort.setSummary(value);
+    	} else if (key.equals(getString(R.string.pref_account_server))) {
+    		value = prefs.getString(key, null);
+    		if (value != null) {
+        		settings.setServer(value);
+    			mServer.setSummary(value);
+    		}
+    	} else if (key.equals(getString(R.string.pref_security_otr_mode))) {
+    		settings.setOtrMode(prefs.getString(key, "auto"));
+    	} else if (key.equals(getString(R.string.pref_security_allow_plain_auth))) {
+    		settings.setAllowPlainAuth(prefs.getBoolean(key, false));
+    	} else if (key.equals(getString(R.string.pref_security_require_tls))) {
+    		settings.setRequireTls(prefs.getBoolean(key, true));
+    	} else if (key.equals(getString(R.string.pref_security_tls_cert_verify))) {
+    		settings.setTlsCertVerify(prefs.getBoolean(key, true));
+    	} else if (key.equals(getString(R.string.pref_security_do_dns_srv))) {
+    		settings.setDoDnsSrv(prefs.getBoolean(key, true));
+    	} else if (key.equals(getString(R.string.pref_hide_offline_contacts))) {
+    		settings.setHideOfflineContacts(prefs.getBoolean(key, false));
+    	} else if (key.equals(getString(R.string.pref_enable_notifications))) {
+    		settings.setEnableNotification(prefs.getBoolean(key, true));
+    	} else if (key.equals(getString(R.string.pref_notification_vibrate))) {
+    		settings.setVibrate(prefs.getBoolean(key, true));
+    	} else if (key.equals(getString(R.string.pref_notification_sound))){
+    		// TODO sort out notification sound pref
+    		if (!prefs.getBoolean(key, false)) {
+    			settings.setRingtoneURI(null);
+    		}
+    	}
+    	settings.close();
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
-        Intent intent = getIntent();
-        mProviderId = intent.getLongExtra(ImServiceConstants.EXTRA_INTENT_PROVIDER_ID, -1);
-        if (mProviderId < 0) {
-            Log.e(ImApp.LOG_TAG,"SettingActivity intent requires provider id extra");
-            throw new RuntimeException("SettingActivity must be created with an provider id");
-        }
-        setInitialValues();
+    	android.os.Debug.waitForDebugger();
+    	super.onCreate(savedInstanceState);
+    	addPreferencesFromResource(R.xml.preferences);
+    	Intent intent = getIntent();
+    	mAccountId = intent.getLongExtra(ImServiceConstants.EXTRA_INTENT_ACCOUNT_ID, -1);
+    	mProviderId = intent.getLongExtra(ImServiceConstants.EXTRA_INTENT_PROVIDER_ID, -1);
+    	if (mProviderId < 0) {
+    		Log.e(ImApp.LOG_TAG,"SettingActivity intent requires provider id extra");
+    		throw new RuntimeException("SettingActivity must be created with an provider id");
+    	}
+    	mUser = (EditTextPreference) findPreference(getString(R.string.pref_account_user));
+    	mPass = (EditTextPreference) findPreference(getString(R.string.pref_account_pass));
+    	mDomain = (EditTextPreference) findPreference(getString(R.string.pref_account_domain));
+    	mXmppResource = (EditTextPreference) findPreference(getString(R.string.pref_account_xmpp_resource));
+    	mPort = (EditTextPreference) findPreference(getString(R.string.pref_account_port));
+    	mServer = (EditTextPreference) findPreference(getString(R.string.pref_account_server));
+    	mOtrMode = (ListPreference) findPreference(getString(R.string.pref_security_otr_mode));
+    	mAllowPlainAuth = (CheckBoxPreference) findPreference(getString(R.string.pref_security_allow_plain_auth));
+    	mRequireTls = (CheckBoxPreference) findPreference(getString(R.string.pref_security_require_tls));
+    	mTlsCertVerify = (CheckBoxPreference) findPreference(getString(R.string.pref_security_tls_cert_verify));
+    	mDoDnsSrv = (CheckBoxPreference) findPreference(getString(R.string.pref_security_do_dns_srv));
+    	mHideOfflineContacts = (CheckBoxPreference) findPreference(getString(R.string.pref_hide_offline_contacts));
+    	mEnableNotification = (CheckBoxPreference) findPreference(getString(R.string.pref_notification_ringtone));
+    	mNotificationVibrate = (CheckBoxPreference) findPreference(getString(R.string.pref_notification_vibrate));
+    	mNotificationSound = (CheckBoxPreference) findPreference(getString(R.string.pref_notification_sound));
+
+    	setInitialValues();
     }
 
-    private void setInitialValues() {
-        Imps.ProviderSettings.QueryMap settings = new Imps.ProviderSettings.QueryMap(
-                getContentResolver(), mProviderId,
-                false /* keep updated */, null /* no handler */);
-        //android.os.Debug.waitForDebugger();
-        
-        // TODO set all preferences here so that the values from the Imps are in the fields, this is needed to support multiple accounts
-        ((EditTextPreference) findPreference(getString(R.string.pref_account_domain))).setText(settings.getDomain());
-        ((EditTextPreference) findPreference(getString(R.string.pref_account_xmpp_resource))).setText(settings.getXmppResource());
-        ((EditTextPreference) findPreference(getString(R.string.pref_account_port))).setText(Integer.toString(settings.getPort()));
-        ((EditTextPreference) findPreference(getString(R.string.pref_account_server))).setText(settings.getServer());
-
-        CheckBoxPreference checkbox; 
-        checkbox = (CheckBoxPreference) findPreference(getString(R.string.pref_security_allow_plain_auth));
-        checkbox.setChecked(settings.getAllowPlainAuth());
-        checkbox = (CheckBoxPreference) findPreference(getString(R.string.pref_security_require_tls));
-        checkbox.setChecked(settings.getRequireTls());
-        checkbox = (CheckBoxPreference) findPreference(getString(R.string.pref_security_tls_cert_verify));
-        checkbox.setChecked(settings.getTlsCertVerify());
-        checkbox = (CheckBoxPreference) findPreference(getString(R.string.pref_security_do_dns_srv));
-        checkbox.setChecked(settings.getDoDnsSrv());
-        checkbox = (CheckBoxPreference) findPreference(getString(R.string.pref_hide_offline_contacts));
-        checkbox.setChecked(settings.getHideOfflineContacts());
-        checkbox = (CheckBoxPreference) findPreference(getString(R.string.pref_enable_notifications));
-        checkbox.setChecked(settings.getEnableNotification());
-        checkbox = (CheckBoxPreference) findPreference(getString(R.string.pref_notification_vibrate));
-        checkbox.setChecked(settings.getVibrate());
-        checkbox = (CheckBoxPreference) findPreference(getString(R.string.pref_notification_sound));
-        checkbox.setChecked(settings.getRingtoneURI() != null);
-        
-        settings.close();
-    }
-
-    /* save the preferences in Imps so they are accessible everywhere
-     * 
-     * (non-Javadoc)
-     * @see android.preference.PreferenceActivity#onPreferenceTreeClick(android.preference.PreferenceScreen, android.preference.Preference)
-     */
     @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        final Imps.ProviderSettings.QueryMap settings = new Imps.ProviderSettings.QueryMap(
-                getContentResolver(), mProviderId,
-                false /* don't keep updated */, null /* no handler */);
-        boolean ret = false;
-        String key = preference.getKey();
-        if (preference instanceof CheckBoxPreference) {
-            boolean value = ((CheckBoxPreference) preference).isChecked();
-            if (key.equals(getString(R.string.pref_security_allow_plain_auth))) {
-                settings.setAllowPlainAuth(value);
-            } else if (key.equals(getString(R.string.pref_security_require_tls))) {
-                settings.setRequireTls(value);
-            } else if (key.equals(getString(R.string.pref_security_tls_cert_verify))) {
-                settings.setTlsCertVerify(value);
-            } else if (key.equals(getString(R.string.pref_security_do_dns_srv))) {
-                settings.setDoDnsSrv(value);
-            } else if (key.equals(getString(R.string.pref_hide_offline_contacts))) {
-                settings.setHideOfflineContacts(value);
-            } else if (key.equals(getString(R.string.pref_enable_notifications))) {
-                settings.setEnableNotification(value);
-            } else if (key.equals(getString(R.string.pref_notification_vibrate))) {
-                settings.setVibrate(value);
-            } else if (key.equals(getString(R.string.pref_notification_sound))){
-                if (!value) {
-                    settings.setRingtoneURI(null);
-                }
-            }
-            ret = true;
-        } else if (preference instanceof EditTextPreference) {
-        	String value = ((EditTextPreference) preference).getText();
-            if (key.equals(getString(R.string.pref_account_user))) {
-                ContentResolver cr = getContentResolver();
-                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-                String password = prefs.getString(getString(R.string.pref_account_pass), null);
-                mAccountId = ImApp.insertOrUpdateAccount(cr, mProviderId, value, password);
-            } else if (key.equals(getString(R.string.pref_account_pass))) {
-                ContentResolver cr = getContentResolver();
-                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-                String userName = prefs.getString(getString(R.string.pref_account_user), null);
-                mAccountId = ImApp.insertOrUpdateAccount(cr, mProviderId, userName, value);
-            } else if (key.equals(getString(R.string.pref_account_domain))) {
-            	settings.setDomain(value);
-            } else if (key.equals(getString(R.string.pref_account_xmpp_resource))) {
-            	settings.setXmppResource(value);
-            } else if (key.equals(getString(R.string.pref_account_port))) {
-            	try {
-            		settings.setPort(Integer.parseInt(value));
-            	} catch (NumberFormatException nfe) {
-            		// TODO port numbers with non-int content should be handled better
-            		Toast.makeText(getBaseContext(), "Port number must be a number",
-            				Toast.LENGTH_SHORT).show();     
-            	}
-            } else if (key.equals(getString(R.string.pref_account_server))) {
-                settings.setServer(value);
-            } else if (key.equals(getString(R.string.pref_security_otr_mode))) {
-            	settings.setOtrMode(value);
-            }
-            ret = true;
-        }
-        settings.close();
-        return ret;
+    protected void onResume() {
+    	super.onResume();
+
+    	setInitialValues();
+    	getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
     }
+
+    @Override
+    protected void onPause() {
+    	super.onPause();
+
+    	getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);    
+    }
+
 }
