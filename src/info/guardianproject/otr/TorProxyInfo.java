ec6679a68f3b2d23c48ba0a52fbcb96063a923ed
Nathan Freitas <nathan@freitas.net>
2011-04-20 20:33:14 -0400

    made this to hold default values for tor proxy

---
+++ src/info/guardianproject/otr/TorProxyInfo.java
@@ -0,0 +1,8 @@
+package info.guardianproject.otr;
+
+public class TorProxyInfo {
+
+	public final static String PROXY_TYPE = "SOCKS5";
+	public final static String PROXY_HOST = "127.0.0.1";
+	public final static int PROXY_PORT = 9050;
+}
