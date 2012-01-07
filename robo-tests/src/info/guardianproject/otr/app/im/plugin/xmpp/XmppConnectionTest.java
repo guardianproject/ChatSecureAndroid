9a44172aef626d5ae5ced7c67c392b7b04741c0a
devrandom <c1.android@niftybox.net>
2012-01-07 15:37:12 -0800

    Fix test and whitespace

--- robo-tests/src/info/guardianproject/otr/app/im/plugin/xmpp/XmppConnectionTest.java
+++ robo-tests/src/info/guardianproject/otr/app/im/plugin/xmpp/XmppConnectionTest.java
@@ -23,6 +23,8 @@ import static junit.framework.Assert.*;
 
 import info.guardianproject.otr.app.im.app.ImApp;
 import info.guardianproject.otr.app.im.engine.Contact;
+import info.guardianproject.otr.app.im.engine.ContactList;
+import info.guardianproject.otr.app.im.engine.ContactListListener;
 import info.guardianproject.otr.app.im.engine.SubscriptionRequestListener;
 import info.guardianproject.otr.app.im.plugin.xmpp.XmppConnection;
 import info.guardianproject.otr.app.im.plugin.xmpp.XmppConnection.MyXMPPConnection;
@@ -46,79 +48,97 @@ import com.xtremelabs.robolectric.RobolectricTestRunner;
  */
 @RunWith(RobolectricTestRunner.class)
 public class XmppConnectionTest extends EasyMockSupport {
-  private static final String DEFAULT_GROUP_NAME = "Buddies";
-  private static final String TEST_CONTACT_NAME = "test";
-  private static final String TEST_CONTACT = "test@test.com";
-  private XmppConnection con;
-  private Roster roster;
-  private MyXMPPConnection smackCon;
-  private ImApp app;
-  private XmppContactList contactListManager;
-  private SubscriptionRequestListener subscriptionRequestListener;
+	private static final String DEFAULT_GROUP_NAME = "Buddies";
+	private static final String TEST_CONTACT_NAME = "test";
+	private static final String TEST_CONTACT = "test@test.com";
+	private XmppConnection con;
+	private Roster roster;
+	private MyXMPPConnection smackCon;
+	private ImApp app;
+	private XmppContactList contactListManager;
+	private SubscriptionRequestListener subscriptionRequestListener;
+	private ContactListListener listener;
 
-  @Before
-  public void setUp() throws Exception {
-	  app = new ImApp();
-	  con = new XmppConnection(app);
-	  roster = createMock(Roster.class);
-	  smackCon = createMock(MyXMPPConnection.class);
-	  expect(smackCon.getRoster()).andStubReturn(roster);
-	  Contact user = new Contact(new XmppConnection.XmppAddress(TEST_CONTACT), TEST_CONTACT_NAME);
-	  con.initConnection(smackCon, user, XmppConnection.LOGGED_IN);
-	  contactListManager = con.getContactListManager();
-	  subscriptionRequestListener = createMock(SubscriptionRequestListener.class);
-	  contactListManager.setSubscriptionRequestListener(subscriptionRequestListener);
-  }
+	@Before
+	public void setUp() throws Exception {
+		app = new ImApp();
+		con = new XmppConnection(app);
+		roster = createMock(Roster.class);
+		smackCon = createMock(MyXMPPConnection.class);
+		expect(smackCon.getRoster()).andStubReturn(roster);
+		Contact user = new Contact(new XmppConnection.XmppAddress(TEST_CONTACT), TEST_CONTACT_NAME);
+		con.initConnection(smackCon, user, XmppConnection.LOGGED_IN);
+		contactListManager = con.getContactListManager();
+		subscriptionRequestListener = createMock(SubscriptionRequestListener.class);
+		contactListManager.setSubscriptionRequestListener(subscriptionRequestListener);
+		listener = createMock(ContactListListener.class);
+		contactListManager.addContactListListener(listener);
+	}
 
-  @Test
-  public void testApproveSubscription() throws Exception {
-	  Capture<RosterListener> listenerCapture = new Capture<RosterListener>();
-	  roster.addRosterListener(capture(listenerCapture));
-	  expectLastCall();
-	  subscriptionRequestListener.onSubscriptionApproved(TEST_CONTACT);
-	  expectLastCall().times(2);
-	  smackCon.sendPacket(anyObject(Packet.class));
-	  expectLastCall();
-	  expect(roster.getGroups()).andReturn(new ArrayList<RosterGroup>());
-	  expect(roster.getUnfiledEntryCount()).andStubReturn(0);
-	  roster.createEntry(eq(TEST_CONTACT), eq(TEST_CONTACT_NAME), aryEq(new String[] { DEFAULT_GROUP_NAME }));
-	  expectLastCall();
-	  replayAll();
-	  contactListManager.listenToRoster(roster);
-	  contactListManager.loadContactLists();
-	  contactListManager.approveSubscriptionRequest(TEST_CONTACT);
-	  // Second time should not call roster.createEntry and such, since contact already exists
-	  contactListManager.approveSubscriptionRequest(TEST_CONTACT);
-	  assertEquals(1, contactListManager.getContactLists().size());
-	  assertNotNull(contactListManager.getContactList(DEFAULT_GROUP_NAME));
-	  verifyAll();
-  }
+	@Test
+	public void testApproveSubscription() throws Exception {
+		expectInitialListCreation();
+		Capture<RosterListener> listenerCapture = new Capture<RosterListener>();
+		roster.addRosterListener(capture(listenerCapture));
+		expectLastCall();
+		subscriptionRequestListener.onSubscriptionApproved(TEST_CONTACT);
+		expectLastCall().times(2);
+		smackCon.sendPacket(anyObject(Packet.class));
+		expectLastCall().times(2);
+		expect(roster.getGroups()).andReturn(new ArrayList<RosterGroup>());
+		expect(roster.getUnfiledEntryCount()).andStubReturn(0);
+		roster.createEntry(eq(TEST_CONTACT), eq(TEST_CONTACT_NAME), aryEq(new String[] { DEFAULT_GROUP_NAME }));
+		expectLastCall().times(2);
+		listener.onContactChange(eq(ContactListListener.LIST_CONTACT_ADDED), anyObject(ContactList.class), anyObject(Contact.class));
+		expectLastCall();
+		replayAll();
+		contactListManager.listenToRoster(roster);
+		contactListManager.loadContactLists();
+		contactListManager.approveSubscriptionRequest(TEST_CONTACT);
+		// Second time should not call notifyContactListUpdated, since contact already exists
+		contactListManager.approveSubscriptionRequest(TEST_CONTACT);
+		assertEquals(1, contactListManager.getContactLists().size());
+		assertNotNull(contactListManager.getContactList(DEFAULT_GROUP_NAME));
+		verifyAll();
+	}
 
-  // Approve a subscription while the server already has a Buddies group
-  @Test
-  public void testApproveSubscription_serverBuddies() throws Exception {
-	  Capture<RosterListener> listenerCapture = new Capture<RosterListener>();
-	  roster.addRosterListener(capture(listenerCapture));
-	  expectLastCall();
-	  subscriptionRequestListener.onSubscriptionApproved(TEST_CONTACT);
-	  expectLastCall();
-	  smackCon.sendPacket(anyObject(Packet.class));
-	  expectLastCall();
-	  final ArrayList<RosterGroup> groups = new ArrayList<RosterGroup>();
-	  RosterGroup buddiesGroup = createNiceMock(RosterGroup.class);
-	  expect(buddiesGroup.getName()).andStubReturn(DEFAULT_GROUP_NAME);
-	  expect(buddiesGroup.getEntries()).andStubReturn(new ArrayList<RosterEntry>());
-	  groups.add(buddiesGroup);
-	  expect(roster.getGroups()).andReturn(groups);
-	  expect(roster.getUnfiledEntryCount()).andStubReturn(0);
-	  roster.createEntry(eq(TEST_CONTACT), eq(TEST_CONTACT_NAME), aryEq(new String[] { DEFAULT_GROUP_NAME }));
-	  expectLastCall();
-	  replayAll();
-	  contactListManager.listenToRoster(roster);
-	  contactListManager.loadContactLists();
-	  contactListManager.approveSubscriptionRequest(TEST_CONTACT);
-	  assertEquals(1, contactListManager.getContactLists().size());
-	  assertNotNull(contactListManager.getContactList(DEFAULT_GROUP_NAME));
-	  verifyAll();
-  }
+	// Approve a subscription while the server already has a Buddies group
+	@Test
+	public void testApproveSubscription_serverBuddies() throws Exception {
+		expectInitialListCreation();
+		Capture<RosterListener> listenerCapture = new Capture<RosterListener>();
+		roster.addRosterListener(capture(listenerCapture));
+		expectLastCall();
+		subscriptionRequestListener.onSubscriptionApproved(TEST_CONTACT);
+		expectLastCall();
+		smackCon.sendPacket(anyObject(Packet.class));
+		expectLastCall();
+		final ArrayList<RosterGroup> groups = new ArrayList<RosterGroup>();
+		RosterGroup buddiesGroup = createNiceMock(RosterGroup.class);
+		expect(buddiesGroup.getName()).andStubReturn(DEFAULT_GROUP_NAME);
+		expect(buddiesGroup.getEntries()).andStubReturn(new ArrayList<RosterEntry>());
+		groups.add(buddiesGroup);
+		expect(roster.getGroups()).andReturn(groups);
+		expect(roster.getUnfiledEntryCount()).andStubReturn(0);
+		roster.createEntry(eq(TEST_CONTACT), eq(TEST_CONTACT_NAME), aryEq(new String[] { DEFAULT_GROUP_NAME }));
+		expectLastCall();
+		listener.onContactChange(eq(ContactListListener.LIST_CONTACT_ADDED), anyObject(ContactList.class), anyObject(Contact.class));
+		expectLastCall();
+		replayAll();
+		contactListManager.listenToRoster(roster);
+		contactListManager.loadContactLists();
+		contactListManager.approveSubscriptionRequest(TEST_CONTACT);
+		assertEquals(1, contactListManager.getContactLists().size());
+		assertNotNull(contactListManager.getContactList(DEFAULT_GROUP_NAME));
+		verifyAll();
+	}
+
+	private void expectInitialListCreation() {
+		listener.onContactChange(eq(ContactListListener.LIST_CREATED), anyObject(ContactList.class), eq((Contact)null));
+		expectLastCall();
+		listener.onContactsPresenceUpdate(anyObject(Contact[].class));
+		expectLastCall();
+		listener.onAllContactListsLoaded();
+		expectLastCall();
+	}
 }
