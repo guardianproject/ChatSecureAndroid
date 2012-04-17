69182399c5530edab17b4c010f53e40488288535
devrandom <c1.android@niftybox.net>
2012-04-17 15:42:41 -0700

    Auto-connect the first time a network is available (e.g. on-boot).

---
+++ src/info/guardianproject/otr/app/im/app/AutoConnectListener.java
@@ -0,0 +1,30 @@
+package info.guardianproject.otr.app.im.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.util.Log;
+
+/**
+ * Automatically initiate the service and connect when the network comes on,
+ * including on boot.
+ */
+public class AutoConnectListener extends BroadcastReceiver {
+    private static final String TAG = "Gibberbot.AppConnectivityListener";
+    static boolean firstCall = true;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "AppConnectivityListener");
+        if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+            boolean noConnectivity = intent.getBooleanExtra(
+                    ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+            Log.d(TAG, "autostart IM service firstCall=" + firstCall + " noconn=" + noConnectivity);
+            if (firstCall && !noConnectivity) {
+                ImApp.getApplication().startImServiceIfNeed(true);
+                firstCall = false;
+            }
+        }
+    }
+}
