078601d1baea72a3417fc9b9789b28f2ce20a2a0
n8fr8 <nathan@freitas.net>
2012-06-10 00:30:41 -0400

    requestedServer may be an empty string
    fixed problem with DNS serve check not happening properly duet
    to a check if a variable for requestedServer was null, when in
    fact it could be an empty string

--- src/info/guardianproject/otr/app/im/plugin/xmpp/XmppConnection.java
+++ src/info/guardianproject/otr/app/im/plugin/xmpp/XmppConnection.java
@@ -76,7 +76,7 @@ import android.util.Log;
 public class XmppConnection extends ImConnection implements CallbackHandler {
 
     final static String TAG = "GB.XmppConnection";
-    private final static boolean DEBUG_ENABLED = false;
+    private final static boolean DEBUG_ENABLED = true;
 
     private XmppContactList mContactListManager;
     private Contact mUser;
@@ -431,7 +431,7 @@ public class XmppConnection extends ImConnection implements CallbackHandler {
     private void initConnection(String userName, final String password,
             Imps.ProviderSettings.QueryMap providerSettings) throws Exception {
 
-        //		android.os.Debug.waitForDebugger();
+//        		android.os.Debug.waitForDebugger();
 
         boolean allowPlainAuth = providerSettings.getAllowPlainAuth();
         boolean requireTls = providerSettings.getRequireTls();
@@ -444,6 +444,8 @@ public class XmppConnection extends ImConnection implements CallbackHandler {
 
         String domain = providerSettings.getDomain();
         String requestedServer = providerSettings.getServer();
+        if ("".equals(requestedServer))
+            requestedServer = null;
         String xmppResource = providerSettings.getXmppResource();
         mPriority = providerSettings.getXmppResourcePrio();
         int serverPort = providerSettings.getPort();
