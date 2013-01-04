13b0ca1db2755ee5ef3a034171c2e39b5bc1b31b
n8fr8 <nathan@freitas.net>
2013-01-04 10:51:58 +0545

    added global setting to control start on boot

--- src/info/guardianproject/otr/app/im/app/AutoConnectListener.java
+++ src/info/guardianproject/otr/app/im/app/AutoConnectListener.java
@@ -3,7 +3,9 @@ package info.guardianproject.otr.app.im.app;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 /**
@@ -18,10 +20,16 @@ public class AutoConnectListener extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "AppConnectivityListener");
         if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+            
             boolean noConnectivity = intent.getBooleanExtra(
                     ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+            
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+            boolean prefStartOnBoot = prefs.getBoolean("pref_start_on_boot", true); 
+            
             Log.d(TAG, "autostart IM service firstCall=" + firstCall + " noconn=" + noConnectivity);
-            if (firstCall && !noConnectivity) {
+            if (firstCall && !noConnectivity && prefStartOnBoot) {
                 ImApp.getApplication().startImServiceIfNeed(true);
                 firstCall = false;
             }
