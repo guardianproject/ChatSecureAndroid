54396c4b090258238ffb95893cc2364f46d88767
Hans-Christoph Steiner <hans@eds.org>
2013-04-10 17:22:47 -0400

    also update ant build for library projects in external/

--- update-ant-build.sh
+++ update-ant-build.sh
@@ -2,3 +2,6 @@
 
 # make sure your Android SDK tools path is set in SDK_BASE
 android update project --path . --name Gibberbot
+android update project --path external/ActionBarSherlock/library/ --name ActionBarSherlock
+android update project --path external/MemorizingTrustManager --name MemorizingTrustManager
+android update project --path external/OnionKit --name OnionKit
