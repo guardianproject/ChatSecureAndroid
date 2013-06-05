129938be5d3e1791b502033c101f062481cdcab4
Devrandom <c1.devrandom@niftybox.net>
2013-06-04 21:39:03 -0700

    Disable extraSymmKey for now
    Not actually used for TLV 0x100, and it has an issue retrieving remote pubkey

--- src/net/java/otr4j/session/SessionImpl.java
+++ src/net/java/otr4j/session/SessionImpl.java
@@ -434,7 +434,7 @@ public class SessionImpl implements Session {
             }
 
             logger.finest("Decrypted message: \"" + decryptedMsgContent + "\"");
-            extraKey = authContext.getExtraSymmetricKey();
+            // FIXME extraKey = authContext.getExtraSymmetricKey();
 
             // Rotate keys if necessary.
             SessionKeys mostRecent = this.getMostRecentSessionKeys();
