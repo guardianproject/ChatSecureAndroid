7106f740bf0f629ea5c59d79e821c2f19fb46286
n8fr8 <nathan@freitas.net>
2013-06-13 20:50:36 -0400

    fix for cacheword building

--- update-ant-build.sh
+++ update-ant-build.sh
@@ -6,5 +6,5 @@ android update project --path external/ActionBarSherlock/actionbarsherlock -t an
 android update project --path external/MemorizingTrustManager --name MemorizingTrustManager -t android-17 --subprojects
 android update project --path external/OnionKit/libonionkit --name OnionKit -t android-17
 android update project --path external/AndroidPinning --name libpinning -t android-17
-android update project --path external/cacheword --name cacheword -t android-17
+android update project --path external/cacheword/cachewordlib --name cacheword -t android-17
 android update project --path external/SlidingMenu --name sliding -t android-17
