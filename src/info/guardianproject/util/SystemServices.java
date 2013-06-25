473c37ba9d99bc273f0f945431334a57f75fb5c2
Lior Saar <liorsaar1@gmail.com>
2013-06-24 21:13:38 -0700

    System services utils added

---
+++ src/info/guardianproject/util/SystemServices.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package info.guardianproject.util;
+
+import info.guardianproject.otr.app.im.R;
+import info.guardianproject.otr.app.im.app.NewChatActivity;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+/**
+ * 
+ * @author liorsaar
+ * 
+ */
+
+/*
+ * Usage:
+ * String filePath = writeFile() ;
+ * Uri fileUri = SystemService.Scanner.scan( context, filePath ) ; // scan that one file
+ * the notification will launch the target activity with the file uri 
+ * SystemServices.Ntfcation.sent( context, fileUri, NewChatActivity.class ) ;
+ * in the target activity call:
+ * Uri uri = getIntent().getData() ;
+ * SystemServices.Viewer.viewImage( context, uri ) ; 
+ */
+public class SystemServices {
+    static class Ntfcation {
+        public static void send(Context aContext, Uri aUri, Class<Activity> aTargetActivityClass) {
+            NotificationManager mNotificationManager = (NotificationManager)aContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            int icon = R.drawable.ic_gb;
+            CharSequence tickerText = "Secured download completed!"; // TODO string
+            long when = System.currentTimeMillis();
+
+            Notification notification = new Notification(icon, tickerText, when);
+            CharSequence contentTitle = "Gibberbot notification";  // TODO string
+            CharSequence contentText = "A secured file was successfuly downloaded.";  // TODO string
+            Intent notificationIntent = new Intent(aContext, aTargetActivityClass);
+            notificationIntent.setData(aUri); // when the target activity is invoked, extract this uri and call viewImage()
+            PendingIntent contentIntent = PendingIntent.getActivity(aContext, 0, notificationIntent, 0);
+            notification.setLatestEventInfo(aContext, contentTitle, contentText, contentIntent);
+            mNotificationManager.notify(1, notification);
+        }
+    }
+
+    static class Scanner {
+        // after writing the file to sd, invoke this to scan a single file without callback
+        public static Uri scan(Context aContext, String aPath) {
+            File file = new File(aPath);
+            Uri uri = Uri.fromFile(file);
+            Intent scanFileIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri);
+            aContext.sendBroadcast(scanFileIntent);
+            return uri;
+        }
+    }
+
+    static class Viewer {
+        public static void viewImage(Context aContext, Uri aUri) {
+            view(aContext, aUri, "image/*");
+        }
+
+        public static void view(Context aContext, Uri aUri, String aMime) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setDataAndType(aUri, aMime);
+            aContext.startActivity(intent);
+        }
+    }
+
+}
