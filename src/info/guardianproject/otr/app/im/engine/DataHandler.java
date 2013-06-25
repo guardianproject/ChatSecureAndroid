7c19558dd983e85f02e27469e2b53c9c1940bae3
Devrandom <c1.devrandom@niftybox.net>
2013-06-24 19:12:36 -0700

    Primitive file transfer chunking

--- src/info/guardianproject/otr/app/im/engine/DataHandler.java
+++ src/info/guardianproject/otr/app/im/engine/DataHandler.java
@@ -18,6 +18,4 @@ public interface DataHandler {
     void onIncomingResponse(Address us, byte[] value);
 
     void offerData(Address us, String localUri, Map<String, String> headers);
-
-    void getData(Address us, String url, Map<String, String> headers);
 }
