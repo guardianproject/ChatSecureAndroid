d47c197ce4d5db6481e810695d429ec450a96599
Devrandom <c1.devrandom@niftybox.net>
2013-06-28 10:58:26 -0700

    Filename handling

--- src/info/guardianproject/otr/app/im/engine/DataListener.java
+++ src/info/guardianproject/otr/app/im/engine/DataListener.java
@@ -1,9 +1,9 @@
 package info.guardianproject.otr.app.im.engine;
 
 public interface DataListener {
-    void onTransferComplete(String from, String url, byte[] data);
+    void onTransferComplete(Address from, String url, byte[] data);
 
-    void onTransferFailed(String screenName, String url, String reason);
+    void onTransferFailed(Address from, String url, String reason);
 
-    void onTransferProgress(String screenName, String url, float f);
+    void onTransferProgress(Address from, String url, float f);
 }
