5c5abfe185ee977fc7fe95f365213553b142b0ca
n8fr8 <nathan@freitas.net>
2013-07-31 00:36:10 -0400

    updating ant build script for new submodules

--- update-ant-build.sh
+++ update-ant-build.sh
@@ -9,6 +9,9 @@ android update project --path external/AndroidPinning --name libpinning -t andro
 android update project --path external/cacheword/cachewordlib --name cacheword -t android-17
 android update project --path external/SlidingMenu/library --name sliding -t android-17
 android update project --path external/SlideListView/library --name slidelist -t android-17
+android update project --path external/NineOldAndroids/library --name slidelist -t android-17
+android update project --path external/MessageBar/library --name slidelist -t android-17
+android update project --path external/ShowcaseView/library --name slidelist -t android-17
 
 cp libs/android-support-v4.jar external/OnionKit/libonionkit/libs/android-support-v4.jar
 cp libs/android-support-v4.jar external/ActionBarSherlock/actionbarsherlock/libs/android-support-v4.jar
