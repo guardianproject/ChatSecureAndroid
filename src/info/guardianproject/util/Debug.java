202b755aa151a3a9961d9992844fef394021dbd3
n8fr8 <nathan@freitas.net>
2013-08-09 10:07:34 -0400

    small tweak to dev/debug handling code

--- src/info/guardianproject/util/Debug.java
+++ src/info/guardianproject/util/Debug.java
@@ -4,10 +4,12 @@ public class Debug {
     public static final boolean DEBUG_ENABLED = false;
 
     public static void onConnectionStart() {
+        if (DEBUG_ENABLED)
         android.os.Debug.waitForDebugger();
     }
 
     public static void onServiceStart() {
+        if (DEBUG_ENABLED)
         android.os.Debug.waitForDebugger();
     }
 }
