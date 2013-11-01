d7a094e38db466d5e5de8e2c8bd87e340d083b81
Hans-Christoph Steiner <hans@eds.org>
2013-11-01 14:31:30 -0400

    ant: get APK name from app_name in res/values/strings.xml

--- update-ant-build.sh
+++ update-ant-build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 target="android-17"
+projectname=`sed -n 's,.*name="app_name">\(.*\)<.*,\1,p' res/values/strings.xml`
 
 # make sure your Android SDK tools path is set in SDK_BASE
 android update lib-project --path external/ActionBarSherlock/actionbarsherlock --target $target
@@ -12,7 +13,7 @@ android update lib-project --path external/SlidingMenu/library --target $target
 android update lib-project --path external/SlideListView/library --target $target
 android update lib-project --path external/AndroidEmojiInput/library --target $target
 
-android update project --path . --name Gibberbot --target android-17 --subprojects
+android update project --path . --name $projectname --target android-17 --subprojects
 
 cp libs/android-support-v4.jar external/OnionKit/libonionkit/libs/android-support-v4.jar
 cp libs/android-support-v4.jar external/ActionBarSherlock/actionbarsherlock/libs/android-support-v4.jar
