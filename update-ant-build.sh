03171d5540286fefc9a0dab98851f928463571d8
n8fr8 <nathan@freitas.net>
2014-06-18 02:07:42 -0400

    adding new submodule and updating build scripts

--- update-ant-build.sh
+++ update-ant-build.sh
@@ -18,5 +18,6 @@ cp libs/android-support-v4.jar external/OnionKit/libonionkit/libs/android-suppor
 cp libs/android-support-v4.jar external/ActionBarSherlock/actionbarsherlock/libs/android-support-v4.jar
 cp libs/android-support-v4.jar external/SlidingMenu/library/libs/android-support-v4.jar
 cp libs/android-support-v4.jar external/cacheword/cachewordlib/libs/android-support-v4.jar
+cp libs/android-support-v4.jar external/ViewPagerIndicator/library/libs/android-support-v4.jar
 
 cp libs/sqlcipher.jar external/cacheword/cachewordlib/libs/sqlcipher.jar
