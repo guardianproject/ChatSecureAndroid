7fb4c4faf3c4b9bd25d694e8d84b99f9e7f95bfe
Lior Saar <liorsaar1@gmail.com>
2014-08-07 11:31:27 -0700

    iocipher - http streamer inner class

--- src/info/guardianproject/otr/app/im/ui/AudioPlayerActivity.java
+++ src/info/guardianproject/otr/app/im/ui/AudioPlayerActivity.java
@@ -119,7 +119,7 @@ public class AudioPlayerActivity extends Activity {
     }
     
     private void initPlayer(String filename, String mimeType) throws Exception {
-        Uri uri = httpStream(filename, mimeType);
+        Uri uri = new HttpStreamer(filename, mimeType).getUri();
         
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
@@ -149,72 +149,83 @@ public class AudioPlayerActivity extends Activity {
         mediaPlayer.setDataSource(this, uri);
         mediaPlayer.prepare();
     }
-
-    private ServerSocket serverSocket = null;
-
-    private Uri httpStream(final String filename, final String mimeType) throws IOException {
+    
+    class HttpStreamer {
+        private Uri uri;
+        private ServerSocket serverSocket;
         
-        // FIXME generate a random token for security
-        final File file = new File(filename);
-        if (!file.exists()) {
-            throw new IOException("File not found " + filename);
+        public HttpStreamer(String filename, String mimeType) throws IOException {
+            uri = create(filename, mimeType);
         }
-
-        try {
-            if (serverSocket != null)
-                serverSocket.close();
-        } catch (Exception e) {
+        
+        public Uri getUri() {
+            return uri;
         }
-
-        serverSocket = new ServerSocket(0); // use random free port
-        new Thread() {
-            public void run() {
-                try {
-                    while (true) {
-                    Socket socket = serverSocket.accept();
-
-                    byte[] b = new byte[8192];
-                    int len;
-                    
-                    InputStream is = socket.getInputStream();
-                    StringBuilder isb = new StringBuilder();
-                    len = is.read(b);
-                    isb.append(new String(b));
-                    
-                    Log.i(TAG, "request: " + isb.toString());
-                    
-                    StringBuilder sb = new StringBuilder();
-                    sb.append("HTTP/1.1 200\r\n");
-                    sb.append("Content-Type: " + mimeType + "\r\n");
-                    sb.append("Content-Length: " + file.length() + "\r\n\r\n");
-
-                    BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
-
-                    bos.write(sb.toString().getBytes());
-
-                    FileInputStream fis = new FileInputStream(file);
-
-                    int idx = 0;
-
-                    while ((len = fis.read(b)) != -1) {
-                        bos.write(b, 0, len);
-                        idx += len;
-                        Log.d(TAG, "sharing via stream: " + idx);
-                    }
-
-                    fis.close();
-                    bos.flush();
-                    bos.close();
-
-                    socket.close();
-                    }
-                } catch (IOException e) {
-                    Log.d(TAG, "web share error", e);
-                }
+        
+        private Uri create(final String filename, final String mimeType) throws IOException {
+            
+            // FIXME generate a random token for security
+            final File file = new File(filename);
+            if (!file.exists()) {
+                throw new IOException("File not found " + filename);
             }
-        }.start();
 
-        Uri uri = Uri.parse("http://localhost:" + serverSocket.getLocalPort() + file.getAbsolutePath());
-        return uri;
+            try {
+                if (serverSocket != null)
+                    serverSocket.close();
+            } catch (Exception e) {
+            }
+
+            serverSocket = new ServerSocket(0); // use random free port
+            new Thread() {
+                public void run() {
+                    try {
+                        while (true) {
+                        Socket socket = serverSocket.accept();
+
+                        byte[] b = new byte[8192];
+                        int len;
+                        
+                        InputStream is = socket.getInputStream();
+                        StringBuilder isb = new StringBuilder();
+                        len = is.read(b);
+                        isb.append(new String(b));
+                        
+                        Log.i(TAG, "request: " + isb.toString());
+                        
+                        StringBuilder sb = new StringBuilder();
+                        sb.append("HTTP/1.1 200\r\n");
+                        sb.append("Content-Type: " + mimeType + "\r\n");
+                        sb.append("Content-Length: " + file.length() + "\r\n\r\n");
+
+                        BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
+
+                        bos.write(sb.toString().getBytes());
+
+                        FileInputStream fis = new FileInputStream(file);
+
+                        int idx = 0;
+
+                        while ((len = fis.read(b)) != -1) {
+                            bos.write(b, 0, len);
+                            idx += len;
+                            Log.d(TAG, "sharing via stream: " + idx);
+                        }
+
+                        fis.close();
+                        bos.flush();
+                        bos.close();
+
+                        socket.close();
+                        }
+                    } catch (IOException e) {
+                        Log.d(TAG, "web share error", e);
+                    }
+                }
+            }.start();
+
+            Uri uri = Uri.parse("http://localhost:" + serverSocket.getLocalPort() + file.getAbsolutePath());
+            return uri;
+        }        
     }
 }
