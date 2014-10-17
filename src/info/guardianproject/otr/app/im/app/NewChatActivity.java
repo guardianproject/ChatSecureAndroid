03852ecca2061d2ad57b41652fb5153152bb227d
n8fr8 <nathan@freitas.net>
2014-10-17 10:49:37 -0400

    had two tap twice to open chat the first time (fixed!)

--- src/info/guardianproject/otr/app/im/app/NewChatActivity.java
+++ src/info/guardianproject/otr/app/im/app/NewChatActivity.java
@@ -2064,10 +2064,12 @@ public class NewChatActivity extends ActionBarActivity implements View.OnCreateC
                     session = manager.createChatSession(username, isNewChat);
                     if (session != null)
                         mRequestedChatId = session.getId();
-                    else
-                    {
-                        //could not create session
-                    }
+                    
+                    if (!showChat(session.getId())) {
+                        // We have a session, but it's not in the cursor yet
+                        mRequestedChatId = session.getId(); 
+                        session.reInit();
+                    } 
                     
                 } else {
                     // Already have session
