a860bca35fb09bab25062e981d612854ccb523e0
n8fr8 <nathan@freitas.net>
2014-11-14 05:31:13 -0500

    updating windows bat

--- update-ant-build.bat
+++ update-ant-build.bat
@@ -2,13 +2,11 @@
 rem update-ant-build script for Windows Command prompt
 rem make sure PATH environment variable includes Android SDK tools path
 
-set target=android-18
+set target=android-21
 set projectname=ChatSecure
 
 echo Updating Android target...
-call android update lib-project --path external/ActionBarSherlock/actionbarsherlock --target %target%
 call android update lib-project --path external/MemorizingTrustManager --target %target%
-call android update lib-project --path external/OnionKit/libonionkit --target %target%
 call android update lib-project --path external/AndroidPinning --target %target%
 call android update lib-project --path external/cacheword/cachewordlib --target %target%
 call android update lib-project --path external/SlidingMenu/library --target %target%
@@ -17,9 +15,8 @@ call android update lib-project --path external/AndroidEmojiInput/library --targ
 call android update project --path . --name %projectname% --target %target% --subprojects
 
 echo Synchronizing jar files...
-copy "libs\android-support-v4.jar" "external\OnionKit\libonionkit\libs\android-support-v4.jar"
-copy "libs\android-support-v4.jar" "external\ActionBarSherlock\actionbarsherlock\libs\android-support-v4.jar"
 copy "libs\android-support-v4.jar" "external\SlidingMenu\library\libs\android-support-v4.jar"
 copy "libs\android-support-v4.jar" "external\cacheword\cachewordlib\libs\android-support-v4.jar"
+copy "libs\android-support-v4.jar" "external\ViewPagerIndicator\library\libs\android-support-v4.jar"
+copy "libs\android-support-v4.jar" "external\AndroidEmojiInputlibs\android-support-v4.jar"
 
-copy "libs\sqlcipher.jar" "external\cacheword\cachewordlib\libs\sqlcipher.jar"
