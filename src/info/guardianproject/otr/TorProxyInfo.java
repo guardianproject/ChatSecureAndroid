81121cb2d9fbff93422758da6787b600a0fdbe73
unknown <v-thurst@DAL-LT-90W0ZZ1.hfc.corp.local>
2015-04-25 15:23:50 -0500

    Updated to new Android Studio Gradle format

--- src/info/guardianproject/otr/TorProxyInfo.java
+++
@@ -1,8 +0,0 @@
-package info.guardianproject.otr;
-
-public class TorProxyInfo {
-
-    public final static String PROXY_TYPE = "SOCKS5";
-    public final static String PROXY_HOST = "127.0.0.1";
-    public final static int PROXY_PORT = 9050;
-}
